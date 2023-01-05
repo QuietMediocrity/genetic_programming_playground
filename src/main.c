c83fda078338cc2714594f5bc37246a6fe07444e
QuietMediocrity <vovadudyshyn@gmail.com>
2023-01-05 21:51:57 +0200

    Introduced agents.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -8,10 +7,14 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
 #define BOARD_WIDTH 20
 #define BOARD_HEIGHT 20
+
 #define CELL_WIDTH ((float)SCREEN_WIDTH / BOARD_WIDTH)
 #define CELL_HEIGHT ((float)SCREEN_HEIGHT / BOARD_HEIGHT)
+
+#define AGENTS_COUNT 100
 
 void scc(int code) // sdl check code
 {
@@ -41,6 +44,59 @@ void render_board_grid(SDL_Renderer *renderer)
 	}
 }
 
+typedef enum {
+	DIR_RIGHT = 0,
+	DIR_UP,
+	DIR_LEFT,
+	DIR_DOWN,
+} direction;
+
+typedef struct {
+	int pos_x, pos_y;
+	direction direction;
+	int hunder;
+	int health;
+} agent;
+
+typedef enum {
+	AA_NOTHING = 0,
+	AA_STEP,
+	AA_EAT,
+	AA_ATTACK,
+} agent_action;
+
+agent agents[AGENTS_COUNT];
+
+int random_int_range(int low, int high)
+{
+	return rand() % (high - low) + low;
+}
+
+direction random_direction(void)
+{
+	return (direction)random_int_range(0, 4);
+}
+
+agent random_agent(void)
+{
+	agent a = { 0 };
+
+	a.pos_x = random_int_range(0, BOARD_WIDTH);
+	a.pos_y = random_int_range(0, BOARD_HEIGHT);
+	a.direction = random_direction();
+	a.hunder = 100;
+	a.health = 100;
+
+	return a;
+}
+
+void initialize_agents(void)
+{
+	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
+		agents[i] = random_agent();
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	scc(SDL_Init(SDL_INIT_VIDEO));
@@ -51,7 +107,7 @@ int main(int argc, char *argv[])
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	scp(renderer);
-        scc(SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT));
+	scc(SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT));
 
 	int quit = 0;
 	while (!quit) {
