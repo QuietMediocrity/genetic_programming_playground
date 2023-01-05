4a985077c646a32b0b9de9f5ee56b63a954e7a4f
QuietMediocrity <vovadudyshyn@gmail.com>
2023-01-05 19:03:50 +0200

    Rudimentary board grid implementation.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -7,6 +8,10 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+#define BOARD_WIDTH 20
+#define BOARD_HEIGHT 20
+#define CELL_WIDTH ((float)SCREEN_WIDTH / BOARD_WIDTH)
+#define CELL_HEIGHT ((float)SCREEN_HEIGHT / BOARD_HEIGHT)
 
 void scc(int code) // sdl check code
 {
@@ -24,6 +29,18 @@ void scp(const void *ptr) // sdl check pointer
 	}
 }
 
+void render_board_grid(SDL_Renderer *renderer)
+{
+	scc(SDL_SetRenderDrawColor(renderer, 80, 80, 80, 255));
+
+	for (int x = 1; x < BOARD_WIDTH; ++x) {
+		scc(SDL_RenderDrawLine(renderer, x * CELL_WIDTH, 0, x * CELL_WIDTH, SCREEN_HEIGHT));
+	}
+	for (int y = 1; y < BOARD_HEIGHT; ++y) {
+		scc(SDL_RenderDrawLine(renderer, 0, y * CELL_HEIGHT, SCREEN_WIDTH, y * CELL_HEIGHT));
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	scc(SDL_Init(SDL_INIT_VIDEO));
@@ -34,6 +51,7 @@ int main(int argc, char *argv[])
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	scp(renderer);
+        scc(SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT));
 
 	int quit = 0;
 	while (!quit) {
@@ -49,7 +67,10 @@ int main(int argc, char *argv[])
 
 		scc(SDL_SetRenderDrawColor(renderer, 36, 41, 46, 255));
 		scc(SDL_RenderClear(renderer));
-                SDL_RenderPresent(renderer);
+
+		render_board_grid(renderer);
+
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_Quit();
