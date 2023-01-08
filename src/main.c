a4a62a1baaa210aea02f35c976137aca0a6fb10e
QuietMediocrity <vovadudyshyn@gmail.com>
2023-01-08 15:57:28 +0200

    Added first iteration of game_step() implemetation.
    I attached game_step() to a keybind (for now), but it doesn't do
    anything, because all genes are initialized with zeros.
    Also renamed all structs to uppercase. They disrupted my naming
    convention for small variables way too much.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <SDL2/SDL_keycode.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -28,6 +29,20 @@
 static_assert(AGENTS_COUNT + FOOD_COUNT + WALLS_COUNT <= BOARD_WIDTH * BOARD_HEIGHT,
 	      "Too many entities. You won't be able to fit all of them on game board.");
 
+#define GENES_COUNT 24
+
+#define FOOD_HUNGER_RECOVERY 10
+#define ATTACK_DMG 10
+#define RETALIATION_DMG 5
+#define STARTING_HEALTH 100
+#define STARTING_HUNGER 50
+#define LETHAL_HUNGER 100
+#define HUNGER_TICK 5
+
+#define HEX_COLOR(hex_color)                                                                               \
+	((hex_color) >> (2 * 8)) & 0xFF, ((hex_color) >> (1 * 8)) & 0xFF, ((hex_color) >> (0 * 8)) & 0xFF, \
+		((hex_color) >> (3 * 8)) & 0xFF
+
 void scc(int code) // sdl check code
 {
 	if (code < 0) {
@@ -43,10 +58,6 @@ void scp(const void *ptr) // sdl check pointer
 		exit(1);
 	}
 }
-
-#define HEX_COLOR(hex_color)                                                                               \
-	((hex_color) >> (2 * 8)) & 0xFF, ((hex_color) >> (1 * 8)) & 0xFF, ((hex_color) >> (0 * 8)) & 0xFF, \
-		((hex_color) >> (3 * 8)) & 0xFF
 
 void render_board_grid(SDL_Renderer *renderer) {
 	scc(SDL_SetRenderDrawColor(renderer, HEX_COLOR(LINE_COLOR)));
@@ -64,7 +75,7 @@ typedef enum {
 	DIR_UP,
 	DIR_LEFT,
 	DIR_DOWN,
-} direction;
+} Direction;
 
 /*
  * This is used to render pointy triangular agents,
@@ -79,7 +90,7 @@ typedef enum {
  *    | *
  *    1
  */
-float agents_dirs[4][6] = {
+float agent_directions[4][6] = {
 	{ 0.0, 0.0, 1.0, 0.5, 0.0, 1.0 }, // DIR_RIGHT
 	{ 0.0, 1.0, 0.5, 0.0, 1.0, 1.0 }, // DIR_UP
 	{ 1.0, 0.0, 1.0, 1.0, 0.0, 0.5 }, // DIR_LEFT
@@ -88,61 +99,78 @@ float agents_dirs[4][6] = {
 
 // state | environment | action | next_state
 
-typedef int agent_state;
+typedef int AgentState;
 
-typedef enum { ENV_NOTHING = 0, ENV_AGENT, ENV_FOOD } environment;
+typedef enum {
+	ENV_NOTHING = 0,
+	ENV_AGENT,
+	ENV_FOOD,
+	ENV_WALL,
+	ENV_COUNT,
+} Environment;
 
 typedef enum {
 	AA_NOTHING = 0,
 	AA_STEP,
 	AA_EAT,
 	AA_ATTACK,
-} agent_action;
+	AA_TURN_LEFT,
+	AA_TURN_RIGHT,
+} AgentAction;
 
 typedef struct {
-	agent_state current_state;
-	agent_state next_state;
-	environment environment;
-	agent_action action;
-} brain_cell;
+	AgentState current_state;
+	AgentState next_state;
+	Environment environment;
+	AgentAction action;
+} Gene;
 
 typedef struct {
 	size_t count;
-	brain_cell cells[];
-} brain;
+	Gene genes[AGENTS_COUNT];
+} Chromosome;
 
 typedef struct {
 	int x;
 	int y;
-} position;
+} Position;
+
+Position position_directions[4] = {
+	{ 1, 0 }, // DIR_RIGHT
+	{ 0, -1 }, // DIR_UP
+	{ -1, 0 }, // DIR_LEFT
+	{ 0, 1 }, // DIR_DOWN
+};
 
 typedef struct {
-	position pos;
-	direction direction;
-	int hunder;
+	Position pos;
+	Direction direction;
+	AgentState current_state;
+	int hunger;
 	int health;
-} agent;
+} Agent;
 
 typedef struct {
 	int quantity;
-	position pos;
-} food;
+	Position pos;
+} Food;
 
 typedef struct {
-	position pos;
-} wall;
+	Position pos;
+} Wall;
 
 typedef struct {
-	agent agents[AGENTS_COUNT];
-	food food[FOOD_COUNT];
-	wall walls[WALLS_COUNT];
-} game;
+	Agent agents[AGENTS_COUNT];
+	Chromosome chromosomes[AGENTS_COUNT];
+	Food food[FOOD_COUNT];
+	Wall walls[WALLS_COUNT];
+} Game;
 
-bool positions_are_equal(position first, position second) {
+bool positions_are_equal(Position first, Position second) {
 	return first.x == second.x && first.y == second.y;
 }
 
-bool is_cell_empty(const game *game, position pos) {
+bool is_cell_empty(const Game *game, Position pos) {
 	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
 		if (positions_are_equal(game->agents[i].pos, pos)) {
 			return false;
@@ -166,18 +194,18 @@ int random_int_range(int low, int high) {
 	return rand() % (high - low) + low;
 }
 
-direction random_direction(void) {
-	return (direction)random_int_range(0, 4);
+Direction random_direction(void) {
+	return (Direction)random_int_range(0, 4);
 }
 
-position random_position(void) {
-	position result = { random_int_range(0, BOARD_WIDTH), random_int_range(0, BOARD_HEIGHT) };
+Position random_position(void) {
+	Position result = { random_int_range(0, BOARD_WIDTH), random_int_range(0, BOARD_HEIGHT) };
 
 	return result;
 }
 
-position random_empty_position(const game *game) {
-	position result = random_position();
+Position random_empty_position(const Game *game) {
+	Position result = random_position();
 	size_t it = 0;
 	const size_t MAX_IT = 250;
 
@@ -189,27 +217,19 @@ position random_empty_position(const game *game) {
 	return result;
 }
 
-agent random_agent(const game *game) {
-	agent a = { 0 };
-
-	a.pos = random_empty_position(game);
-	a.direction = random_direction();
-	a.hunder = 100;
-	a.health = 100;
-
-	return a;
-}
-
-void initialize_game(game *game) {
+void initialize_game(Game *game) {
 	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
-		game->agents[i] = random_agent(game);
+		game->agents[i].pos = random_empty_position(game);
+		game->agents[i].direction = random_direction();
+		game->agents[i].hunger = STARTING_HUNGER;
+		game->agents[i].health = STARTING_HEALTH;
 
 		// qm_todo: Remove this later.
 		game->agents[i].direction = i % 4;
 	}
 
-	// qm_todo: Yes, they can happen to be on top of each other, but
-	// who cares. Maybe I'll fix it later.
+	// qm_todo: Yes, they can happen to be on top of each other, but who cares.
+	// Maybe I'll fix it later.
 	for (size_t i = 0; i < FOOD_COUNT; ++i) {
 		game->food[i].pos = random_empty_position(game);
 	}
@@ -219,30 +239,41 @@ void initialize_game(game *game) {
 	}
 }
 
-void render_agent(SDL_Renderer *renderer, const game *game, size_t index) {
+void render_agent(SDL_Renderer *renderer, const Game *game, size_t index) {
 	const float AGENT_PADDING = 6.f;
 	const float CELL_WIDTH_PADDING = CELL_WIDTH - AGENT_PADDING * 2;
 	const float CELL_HEIGHT_PADDING = CELL_HEIGHT - AGENT_PADDING * 2;
-	const agent *a = &game->agents[index];
+	const Agent *a = &game->agents[index];
 
-	const float x1 = agents_dirs[a->direction][0] * CELL_WIDTH_PADDING + a->pos.x * CELL_WIDTH + AGENT_PADDING;
-	const float y1 = agents_dirs[a->direction][1] * CELL_HEIGHT_PADDING + a->pos.y * CELL_HEIGHT + AGENT_PADDING;
-	const float x2 = agents_dirs[a->direction][2] * CELL_WIDTH_PADDING + a->pos.x * CELL_WIDTH + AGENT_PADDING;
-	const float y2 = agents_dirs[a->direction][3] * CELL_HEIGHT_PADDING + a->pos.y * CELL_HEIGHT + AGENT_PADDING;
-	const float x3 = agents_dirs[a->direction][4] * CELL_WIDTH_PADDING + a->pos.x * CELL_WIDTH + AGENT_PADDING;
-	const float y3 = agents_dirs[a->direction][5] * CELL_HEIGHT_PADDING + a->pos.y * CELL_HEIGHT + AGENT_PADDING;
+	const float x1 = agent_directions[a->direction][0] * CELL_WIDTH_PADDING + a->pos.x * CELL_WIDTH + AGENT_PADDING;
+	const float y1 =
+		agent_directions[a->direction][1] * CELL_HEIGHT_PADDING + a->pos.y * CELL_HEIGHT + AGENT_PADDING;
+	const float x2 = agent_directions[a->direction][2] * CELL_WIDTH_PADDING + a->pos.x * CELL_WIDTH + AGENT_PADDING;
+	const float y2 =
+		agent_directions[a->direction][3] * CELL_HEIGHT_PADDING + a->pos.y * CELL_HEIGHT + AGENT_PADDING;
+	const float x3 = agent_directions[a->direction][4] * CELL_WIDTH_PADDING + a->pos.x * CELL_WIDTH + AGENT_PADDING;
+	const float y3 =
+		agent_directions[a->direction][5] * CELL_HEIGHT_PADDING + a->pos.y * CELL_HEIGHT + AGENT_PADDING;
 
-	filledTrigonColor(renderer, x1, y1, x2, y2, x3, y3, AGENT_COLOR);
-	aatrigonColor(renderer, x1, y1, x2, y2, x3, y3, AGENT_COLOR);
+	if (a->health > 0) {
+		filledTrigonColor(renderer, x1, y1, x2, y2, x3, y3, AGENT_COLOR);
+		aatrigonColor(renderer, x1, y1, x2, y2, x3, y3, AGENT_COLOR);
+	} else {
+		filledTrigonColor(renderer, x1, y1, x2, y2, x3, y3, DEAD_AGENT_COLOR);
+		aatrigonColor(renderer, x1, y1, x2, y2, x3, y3, DEAD_AGENT_COLOR);
+	}
 }
 
-void render_game(SDL_Renderer *renderer, const game *game) {
+void render_game(SDL_Renderer *renderer, const Game *game) {
 	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
 		render_agent(renderer, game, i);
 	}
 
 	const float FOOD_PADDING = 12.5f;
 	for (size_t i = 0; i < FOOD_COUNT; ++i) {
+		if (game->food[i].quantity == 0)
+			continue;
+
 		filledCircleRGBA(renderer,
 				 (int)floorf(game->food[i].pos.x * CELL_WIDTH + CELL_WIDTH * 0.5f),
 				 (int)floorf(game->food[i].pos.y * CELL_HEIGHT + CELL_HEIGHT * 0.5f),
@@ -264,6 +295,159 @@ void render_game(SDL_Renderer *renderer, const game *game) {
 	}
 }
 
+int mod_int(int first, int second) {
+	return (first % second + second) % second;
+}
+
+Position get_position_infront_of_agent(const Agent *agent) {
+	Position delta = position_directions[agent->direction];
+	Position next = agent->pos;
+
+	next.x += mod_int(next.x + delta.x, BOARD_WIDTH);
+	next.y += mod_int(next.y + delta.y, BOARD_HEIGHT);
+
+	return next;
+}
+
+void move_agent(Agent *agent) {
+	Position delta = position_directions[agent->direction];
+
+	agent->pos.x = mod_int(agent->pos.x + delta.x, BOARD_WIDTH);
+	agent->pos.y = mod_int(agent->pos.y + delta.y, BOARD_HEIGHT);
+}
+
+Food *get_ptr_to_food_infront_of_agent(Game *game, Agent *agent) {
+	Position infront = get_position_infront_of_agent(agent);
+
+	for (size_t i = 0; i < FOOD_COUNT; ++i) {
+		if (positions_are_equal(game->food[i].pos, infront))
+			return &game->food[i];
+	}
+
+	return NULL;
+}
+
+Agent *get_ptr_to_agent_infront_of_agent(Game *game, Agent *agent) {
+	Position infront = get_position_infront_of_agent(agent);
+
+	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
+                // qm_todo: do I need to check if agent[i] == *agent?
+                // when will it be the case?
+		if (positions_are_equal(game->agents[i].pos, infront) && game->agents[i].health > 0)
+			return &game->agents[i];
+	}
+
+	return NULL;
+}
+
+Wall *get_ptr_to_wall_infront_of_agent(Game *game, Agent *agent) {
+	Position infront = get_position_infront_of_agent(agent);
+
+	for (size_t i = 0; i < WALLS_COUNT; ++i) {
+		if (positions_are_equal(game->walls[i].pos, infront))
+			return &game->walls[i];
+	}
+
+	return NULL;
+}
+
+Environment interpret_environment_infront_of_agent(Game *game, size_t agent_index) {
+        Agent *agent = &game->agents[agent_index];
+
+        // This order kind of serves as priority list.
+
+        if (get_ptr_to_food_infront_of_agent(game, agent) != NULL)
+                return ENV_FOOD;
+
+        if (get_ptr_to_agent_infront_of_agent(game, agent) != NULL)
+                return ENV_AGENT;
+
+        if (get_ptr_to_wall_infront_of_agent(game, agent) != NULL)
+                return ENV_WALL;
+
+	return ENV_NOTHING;
+}
+
+void execute_action(Game *game, size_t agent_index, AgentAction action) {
+	Agent *agent = &game->agents[agent_index];
+
+	switch (action) {
+	case AA_NOTHING:
+		break;
+
+	case AA_STEP:
+		if (interpret_environment_infront_of_agent(game, agent_index) != ENV_WALL) {
+			move_agent(agent);
+		}
+		break;
+
+	case AA_EAT: {
+		Food *food = get_ptr_to_food_infront_of_agent(game, agent);
+
+		if (food != NULL) {
+			food->quantity -= 1;
+			agent->hunger -= FOOD_HUNGER_RECOVERY;
+
+			if (agent->hunger < 0)
+				agent->hunger = 0;
+		}
+	} break;
+
+	case AA_ATTACK: {
+		Agent *victim = get_ptr_to_agent_infront_of_agent(game, agent);
+
+		if (victim != NULL) {
+			victim->health -= ATTACK_DMG;
+			agent->health -= RETALIATION_DMG;
+
+			// No check for negative hp here.
+			// We perform all actions first, then declare dead agents.
+		}
+	} break;
+
+	case AA_TURN_LEFT:
+		// this is absolutely brilliant!
+		agent->direction = mod_int(agent->direction + 1, 4);
+		break;
+
+	case AA_TURN_RIGHT:
+		agent->direction = mod_int(agent->direction - 1, 4);
+		break;
+
+	default:
+		assert(0 && "This is not supposed to happen, fix the 'action' value.");
+		break;
+	}
+}
+
+void game_step(Game *game) {
+	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
+		for (size_t j = 0; j < AGENTS_COUNT; ++j) {
+			Gene *gene = &game->chromosomes[i].genes[j];
+
+			if (gene->current_state != game->agents[i].current_state)
+				continue;
+
+			if (gene->environment != interpret_environment_infront_of_agent(game, i))
+				continue;
+
+			if (game->agents[i].health > 0)
+				execute_action(game, i, gene->action);
+
+			game->agents[i].current_state = gene->next_state;
+		}
+	}
+
+	for (size_t i = 0; i < AGENTS_COUNT; ++i) {
+		if (game->agents[i].hunger == LETHAL_HUNGER) {
+			game->agents[i].health -= HUNGER_TICK;
+			continue;
+		}
+
+		game->agents[i].hunger += HUNGER_TICK;
+	}
+}
+
 int main(int argc, char *argv[]) {
 	srand(time(0));
 	scc(SDL_Init(SDL_INIT_VIDEO));
@@ -276,7 +460,7 @@ int main(int argc, char *argv[]) {
 	scp(renderer);
 	// scc(SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT));
 
-	game game;
+	Game game;
 	initialize_game(&game);
 
 	int quit = 0;
@@ -287,6 +471,12 @@ int main(int argc, char *argv[]) {
 			switch (event.type) {
 			case SDL_QUIT: {
 				quit = 1;
+			} break;
+			case SDLK_r: {
+				initialize_game(&game);
+			} break;
+                        case SDLK_s: { // qm_todo: later on, make the ticks automatic after delta t, not manual.
+				game_step(&game);
 			} break;
 			}
 		}
